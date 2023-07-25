c6c9658ba8011fb44197cf74729ab2d06994c183
princebxr02@gmail.com <princebxr02@gmail.com>
2023-07-25 09:42:51 +0530

    Prince

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sgithub-action-automate
---
+++ tf-example/main.tf
@@ -0,0 +1,36 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/*20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name      = "app-ssh-key"
+
+  tags = {
+    Name = var.ec2_name
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-nie-s3-bk8t"  
+  acl    = "private"  
+
+}
+
